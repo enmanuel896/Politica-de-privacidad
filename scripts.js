f2545ad8bb82593213c093ecc407a9742f09c540
enmanuel896 <enmalviarez17@gmail.com>
2021-09-19 23:23:25 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,43 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".navbar", {
+    delay: 0,
+    duration: 1000,
+    origin: "bottom",
+    distance: "-300px",
+});
+
+sr.reveal(".bannerRight", {
+    delay: 500,
+    duration: 2000,
+    origin: "top",
+    distance: "-300px",
+});
+
+sr.reveal(".educacion", {
+    delay: 500,
+    duration: 2000,
+    origin: "bottom",
+    distance: "-50px",
+});
+
+sr.reveal(".ventajas-1", {
+    delay: 1200,
+    duration: 1000,
+    origin: "right",
+    distance: "-300px",
+});
+
+sr.reveal(".ventajas-2", {
+    delay: 1000,
+    duration: 1000,
+    origin: "right",
+    distance: "-50px",
+});
+
+sr.reveal(".desventajas", {
+    delay: 1500,
+    duration: 1500,
+    origin: "top",
+    distance: "-50px",
+});
